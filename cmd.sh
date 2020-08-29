d09f05043c89b7e8979a6089dcf65b5fbaa276c7
kyleabrock <kyle.abrock@gmail.com>
2020-08-29 11:59:53 +0300

    added jenkins

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
     echo "Running development server" # Запуск сервера для разработки
     exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Running unit tests"
+    exec python "tests.py"
 else
     echo "Running production server" # Запуск сервера для тестирования
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
